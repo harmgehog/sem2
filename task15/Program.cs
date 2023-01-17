7dbca9859a533fabdb5aa4166eebf2a2041579c6
harmgehog <polstia@gmail.com>
2023-01-17 15:16:07 +0300

    add switch case for third task

--- task15/Program.cs
+++ task15/Program.cs
@@ -8,3 +8,17 @@
 int num = Convert.ToInt32(Console.ReadLine());
 string rslt = num == 6 || num == 7 ? "да" : "нет";
 Console.WriteLine(rslt);
+
+// another option switch case
+
+switch (num)
+{
+    case 6: 
+        goto case 7;
+    case 7: 
+        Console.WriteLine("да");
+        break;
+    default: 
+        Console.WriteLine("нет");
+        break;
+};
